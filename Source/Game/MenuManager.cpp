b01e2ebad3ff90ec64db9d02434c8e5d7438e184
Oliwer Dahlin <oliwerdahlin@gmail.com>
2021-01-15 10:49:30 +0100

    Menu code added

---
+++ Source/Game/MenuManager.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "MenuManager.h"
+
+namespace Studio
+{
+    MenuObject* Studio::MenuManager::GetMainMenu()
+    {
+        return &myMainMenu;
+    }
+
+    MenuObject* Studio::MenuManager::GetHUD()
+    {
+        return &myHud;
+    }
+
+    MenuObject* Studio::MenuManager::GetPauseMenu()
+    {
+        return &myPausMenu;
+    }
+}
---
+++ Source/Game/MenuObject.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include "UIElement.h"
+
+namespace Studio
+{
+	class MenuObject
+	{
+	public:
+		MenuObject();
+		~MenuObject();
+
+		void Enable();
+		void Disable();
+
+		void Add(UIElement aElementToAdd);
+
+		UIElement* GetElementWithTag(const char* aTag);
+
+	private:
+		std::vector<UIElement> myElements;
+	};
+}
+
