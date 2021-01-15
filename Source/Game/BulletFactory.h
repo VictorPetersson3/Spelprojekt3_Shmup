38a3ce8835a297d34c8f15e6da890d5aab8ce66f
VictorPetersson3 <victor.petersson@studerande.thegameassembly.se>
2021-01-15 16:44:21 +0100

    Bullet Factory and Factory Updates

--- Source/Game/BulletFactory.h
+++ Source/Game/BulletFactory.h
@@ -2,8 +2,19 @@
 #include <map>
 #include "TypePattern_Bullet.h"
 
-
-class BulletFactory
+namespace Studio
 {
-};
+	class Bullet;
+	class BulletFactory
+	{
+	public:
+		BulletFactory() = default;
+		~BulletFactory();
+		void InitBulletType(const std::string & aPath, const unsigned int aLayerOrder, const std::string & aType);
+		Studio::Bullet* CreateBulletObject(const std::string& aType, const Tga2D::Vector2f& aPosition, const float& aSpeed);
+	private:
+		std::map<std::string, TypePattern_Bullet*> myBulletObjects;
+	};
+}
+
 
