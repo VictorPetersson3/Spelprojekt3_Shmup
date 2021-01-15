b723adcd71a098572f96539e0d99cb0e7a5ddaeb
jesper-lundgren <70626985+jesper-lundgren@users.noreply.github.com>
2021-01-15 17:55:08 +0100

    Added Level Manager that preloads a level from level_x.json

--- Source/Game/GameWorld.cpp
+++ Source/Game/GameWorld.cpp
@@ -6,6 +6,7 @@
 #include "Bullet.h"
 #include <tga2d/sprite/sprite.h>
 #include "RendererAccessor.h"
+#include "LevelManager.h"
 
 CGameWorld::CGameWorld()
 {
@@ -18,6 +19,7 @@ CGameWorld::~CGameWorld()
 	delete myTga2dLogoSprite;
 	myTga2dLogoSprite = nullptr;
 	SAFE_DELETE(myPlayer);
+	SAFE_DELETE(myLevelManager);
 }
 
 void CGameWorld::Init()
@@ -28,7 +30,8 @@ void CGameWorld::Init()
 	myTga2dLogoSprite->SetPivot({ 0.5f, 0.5f });
 	myTga2dLogoSprite->SetPosition({ 0.5f, 0.5f });
 	SAFE_CREATE(myPlayer, Studio::Player(new Tga2D::CSprite("sprites/debugpixel.dds")));
-	
+	SAFE_CREATE(myLevelManager, Studio::LevelManager());
+
 	for (int i = 0; i < 1; i++)
 	{
 		auto enemy = new Studio::Enemy(new Tga2D::CSprite("sprites/debugpixel.dds"), { 0.9f, 0.5f });
@@ -40,12 +43,15 @@ void CGameWorld::Init()
 //aIsPlaying is an atomic bool to close the gameplay thread
 void CGameWorld::Update(float aDeltaTime, std::atomic<bool>& aIsPlaying)
 {
+
 	UpdatePlayer(aDeltaTime);
 
 	UpdateEnemies(aDeltaTime);
 
 	CheckIfEnemiesHit();
-	
+
+	myLevelManager->Update();
+
 	myBackgroundManager.UpdateBackground(aDeltaTime);
 }
 
