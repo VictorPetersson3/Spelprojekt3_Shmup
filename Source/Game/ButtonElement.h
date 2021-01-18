6b8d3e44b4804abd33f320079006b3eb1d05e503
Yoghurtfresse <jimmi00@outlook.com>
2021-01-18 15:39:39 +0100

    Revert "Merge conflict fixed"
    This reverts commit ce3aeab0209ba8eda19d08eee53b62bc64514e14.

--- Source/Game/ButtonElement.h
+++ Source/Game/ButtonElement.h
@@ -12,17 +12,18 @@ namespace Studio
 	class ButtonElement : public UIElement
 	{
 	public:
+<<<<<<< HEAD
 		ButtonElement();
 		ButtonElement(const char* aID,const char* aPath, VECTOR2F aPosition, VECTOR2F aSize, VECTOR2F aPivot);
+=======
+		ButtonElement(const char* aPath, VECTOR2F aPosition, VECTOR2F aSize);
+>>>>>>> parent of 4a642b0... Audio and Ui updates
 		~ButtonElement();
-
 		bool IsClicked() override;
 
 		void Render()override;
 
 		void ResetButton();
-
-		void OnClick();
 
 	protected:
 		Tga2D::CSprite* mySprite;
