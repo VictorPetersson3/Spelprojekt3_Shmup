503901301a3183b7d18afbf079706af26cb4fb65
VictorPetersson3 <victor.petersson@studerande.thegameassembly.se>
2021-01-19 16:58:12 +0100

    I fixed so the manager handles the new Pixel Sorting Stuff

--- Source/Game/BackgroundManager.cpp
+++ Source/Game/BackgroundManager.cpp
@@ -4,31 +4,32 @@
 #include "RendererAccessor.h"
 #include "Renderer.h"
 #include "BackgroundObject.h"
-//#include <iostream>
+#include <iostream>
 
 void Studio::BackgroundManager::CreateTestMapBackground(float LevelWidth)
 {
-	myLevelWidth = LevelWidth / static_cast<float>(Tga2D::CEngine::GetInstance()->GetWindowSize().x);
+	myLevelWidth = LevelWidth;
 	myBackgroundFactory.InitBackGroundObject("sprites/Background/Background_Background_01.dds", 0, "Background");
 	myBackgroundFactory.InitBackGroundObject("sprites/Background/Background_MountainRange_TileAble.dds", 1, "Mountains");
 	myBackgroundFactory.InitBackGroundObject("sprites/Background/Background_GradientBackground_01.dds", 2, "Gradient");
 	myBackgroundFactory.InitBackGroundObject("sprites/Background/Background_CloudTest_01.dds", 3, "Clouds");
 
-	myBackgroundObjects.push_back(myBackgroundFactory.CreateBackgroundObject("Background", Tga2D::Vector2f({ 0.5, 0.5 }), 0, Tga2D::Vector2f({ 1.1, 1.1 })));
+	myBackgroundObjects.push_back(myBackgroundFactory.CreateBackgroundObject("Background", Tga2D::Vector2f({ 960, 540 }), 0, Tga2D::Vector2f({ 1.1, 1.1 })));
 	
 	//Sätt in if statments för dem olika typerna så jag använder rätt överlagring osv
 	
 	for (int i = 0; i < 10; i++)
 	{
-		myBackgroundObjects.push_back(myBackgroundFactory.CreateBackgroundObject("Mountains", Tga2D::Vector2f({((512.f / static_cast<float>(Tga2D::CEngine::GetInstance()->GetRenderSize().x))) * i, 0.873f }), -0.025f));
+		myBackgroundObjects.push_back(myBackgroundFactory.CreateBackgroundObject("Mountains", Tga2D::Vector2f({512.f * i, 943.f}), -50.0f));
+		std::cout << "Mountain X: " << 512.f * i << std::endl;
 	}
-	myBackgroundObjects.push_back(myBackgroundFactory.CreateBackgroundObject("Gradient", Tga2D::Vector2f({ 0.5, 0.5 }), 0, Tga2D::Vector2f({ 1.1, 1.1 })));
+	myBackgroundObjects.push_back(myBackgroundFactory.CreateBackgroundObject("Gradient", Tga2D::Vector2f({ 960, 540 }), 0, Tga2D::Vector2f({ 1.1, 1.1 })));
 
 	for (int i = 0; i < 20; i++)
 	{
 		myBackgroundObjects.push_back(myBackgroundFactory.CreateBackgroundObject("Clouds", 
-			Tga2D::Vector2f({ CommonUtilities::GetRandomFloat(0, myLevelWidth), CommonUtilities::GetRandomFloat(0.f, 0.6f) }),
-			CommonUtilities::GetRandomFloat(0.04f, 0.1f) * (-1.f)));
+			Tga2D::Vector2f({ CommonUtilities::GetRandomFloat(0, myLevelWidth), CommonUtilities::GetRandomFloat(0.f, 600.f) }),
+			CommonUtilities::GetRandomFloat(-100.f, -20.f) )); 
 	}
 }
 
@@ -36,10 +37,10 @@ void Studio::BackgroundManager::CreateTestMapBackground(float LevelWidth)
 
 void Studio::BackgroundManager::UpdateBackground(float aDeltaTime)
 {
-	for (int i = myBackgroundObjects.size(); i-- > 0;)
+	for (int i = 0; i < myBackgroundObjects.size(); i++)
 	{
 		myBackgroundObjects.at(i)->Update(aDeltaTime);
-		if (myBackgroundObjects.at(i)->GetPosition().x < -0.5)
+		if (myBackgroundObjects.at(i)->GetPosition().x < -513.f)
 		{
 			myBackgroundObjects.at(i)->SetPosition({ myBackgroundObjects.at(i)->GetPosition().x + myLevelWidth, myBackgroundObjects.at(i)->GetPosition().y });
 		}
