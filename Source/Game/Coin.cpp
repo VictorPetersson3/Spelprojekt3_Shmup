c42064096604d88d66dead080f06576f08868690
Yoghurtfresse <jimmi00@outlook.com>
2021-01-28 16:39:19 +0100

    Coin now animated!

--- Source/Game/Coin.cpp
+++ Source/Game/Coin.cpp
@@ -1,15 +1,20 @@
 #include "stdafx.h"
 #include "Coin.h"
 
+#define SPRITESHEET GameObject::GetSpriteSheet()
+
 namespace Studio
 {
-    Coin::Coin(VECTOR2F aSpawnPosition) : GameObject("Sprites/Pickups/coin1.dds")
+    Coin::Coin(VECTOR2F aSpawnPosition) : GameObject("Sprites/Pickups/coinsprite.dds")
     {
+        SPRITESHEET.SetAmountOfFrames({ 8, 1 });
         myPosition = aSpawnPosition;
-        mySpriteSheet.SetImagePath("Sprites/Pickups/coin1.dds");
+        mySpriteSheet.SetImagePath("Sprites/Pickups/coinsprite.dds");
         GameObject::myCollider.AddCircleColliderObject(myPosition, 16);
+        mySpriteSheet.LoopAnimationInRange(0.1f, { 1,1 }, { 6,1 });
         myScoreValue = 1;
     }
+
 
     int Coin::GetScoreValue()
     {
