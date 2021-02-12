7aca75e0570b7eefd67f1d870a8f997eaeba2567
jesper-lundgren <70626985+jesper-lundgren@users.noreply.github.com>
2021-02-12 16:00:30 +0100

    Made explosions larger

--- Source/Game/LevelManager.h
+++ Source/Game/LevelManager.h
@@ -31,7 +31,7 @@ namespace Studio
 		void SpawnMissile(const Enums::BulletOwner& aOwner, const Tga2D::Vector2f& aPosition, const float aExplosionRadius, const float aDamageAmount, const float aExplosionDamageAmount);
 		void SpawnAOEBullet(const Enums::BulletOwner& aOwner, const Tga2D::Vector2f& aPosition, const float aRadius);
 		void SpawnTimedBomb(const Tga2D::Vector2f& aPosition, const Tga2D::Vector2f& aVelocity, const float aBlastRadius, const float aDamage);
-		void CreateExplosionAt(const Tga2D::Vector2f& aPosition, const float aRadius = 20.0f);
+		void CreateExplosionAt(const Tga2D::Vector2f& aPosition, const float aRadius = 50.0f);
 		bool LevelIsCleared();
 		void LoadLevel(const int aLevelIndex);
 		void ReloadLevel();
