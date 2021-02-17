50f0de44318b04e44602f90ca169e27c826943e4
PuEkstrand <puekstrand1@gmail.com>
2021-02-17 14:46:43 +0100

    Boss laser :flashlight:
    Added laser animation :D

--- Source/Game/Laser.cpp
+++ Source/Game/Laser.cpp
@@ -2,14 +2,16 @@
 #include "Laser.h"
 #include "RendererAccessor.h"
 #include "Renderer.h"
-Studio::Laser::Laser(VECTOR2F aPosition) : 
-	GameObject("Sprites/Bullets/redlong.dds")
+Studio::Laser::Laser(VECTOR2F aPosition) :
+	GameObject("Sprites/assets/enemies/boss/laserSprite_02.dds")
 {
 	//Figure out a better way to do an offset...
-	myPosition = { aPosition.x - 969.0f, aPosition.y - 63.0f };
+	mySpriteSheet.SetAmountOfFrames({ 2, 16 });
+	mySpriteSheet.PlayAnimationInRange(0.082333f, { 1, 1 }, { 2, 10 });
+	myPosition = { aPosition.x -1250.0f, aPosition.y - 55.0f };
 	myCollider.AddBoxColliderObject({ 0.0f, 0.0f }, { 2000.0f, 25.0f });
-	myDamage = 1.0f;
-	mySpriteSheet.SetSizeRelativeToImage({25.0f, 1.25f});
+	myDamage = 0.0f;
+	//mySpriteSheet.SetSizeRelativeToImage({ -1, 1 });
 }
 
 Studio::Laser::~Laser()
@@ -19,6 +21,11 @@ Studio::Laser::~Laser()
 void Studio::Laser::Update(float anY)
 {
 	GameObject::Update({ myPosition.x, anY - 63.0f });
+	if (!mySpriteSheet.IsAnimating())
+	{
+		mySpriteSheet.LoopAnimationInRange(0.082333f, { 1, 11 }, {2, 12 });
+		myDamage = 1.0f;
+	}
 	RendererAccessor::GetInstance()->Render(*this);
 }
 
