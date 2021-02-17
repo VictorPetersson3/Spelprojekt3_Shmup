aab38e3a37dfce86cfd7565d5d14db0ad2d8e0e8
Oliwer Dahlin <oliwerdahlin@gmail.com>
2021-02-17 15:35:48 +0100

    Fixed shop

--- Source/Game/ShopUI.cpp
+++ Source/Game/ShopUI.cpp
@@ -9,38 +9,40 @@
 
 int Studio::ShopUI::GetRandomNumberInRange(int max)
 {
-	return CommonUtilities::GetRandomInt(0, max);
+	return CommonUtilities::RandNumbInRange(0, max);
 }
 
 Studio::ShopUI::ShopUI()
 {
 
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_AdrenalineRush_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::RapidFireAttackSpeed, 5, "Sprites/UI/ShopUI/RapidFireAST1.dds", "Adrenaline Rush Tier 1"));
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_DrumMagazine_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::RapidFireDuration, 5, "Sprites/UI/ShopUI/RapidFireDUT1.dds", "Drum Magazine Tier 1"));
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_RapidReload_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::RapidFireCooldown, 5, "Sprites/UI/ShopUI/RapidFireCDT1.dds", "Rapid Reload Tier 1"));
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_ExtendedChargerCable_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::ShieldCooldown, 5, "Sprites/UI/ShopUI/ShieldCDT1.dds", "Extended Charger Cable Tier 1"));
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_StormReaktor_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::ShieldDuration, 5, "Sprites/UI/ShopUI/ShieldDUT1.dds","Storm Reaktor Tier 1"));
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_5T0NK5BatteryCore_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::ShieldHealth, 5, "Sprites/UI/ShopUI/ShieldHPT1.dds","5TONK% Battery Core Tier 1"));
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_Triggerhappy_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::BasicAttackSpeed, 5, "Sprites/UI/ShopUI/BasicAttackSpeedT1.dds","Trigger Happy Tier 1"));
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_ArkReaktor_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::MissileCooldown, 5, "Sprites/UI/ShopUI/MissileCDRT1.dds","Ark Reaktor Tier 1"));
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_GrannysDoughRecipe_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::MissileDamage, 5, "Sprites/UI/ShopUI/MissileDMGT1.dds","Grannys Dough Recipe Tier 1"));
-	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_BluesprintEnlarger_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::MissileExplosionRadius, 5, "Sprites/UI/ShopUI/MissileRADT1.dds","Bluesprint Enlarger Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_AdrenalineRush_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::RapidFireAttackSpeed, 10, "Sprites/UI/ShopUI/RapidFireAST1.dds", "Adrenaline Rush Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_DrumMagazine_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::RapidFireDuration, 10, "Sprites/UI/ShopUI/RapidFireDUT1.dds", "Drum Magazine Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_RapidReload_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::RapidFireCooldown, 10, "Sprites/UI/ShopUI/RapidFireCDT1.dds", "Rapid Reload Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_ExtendedChargerCable_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::ShieldCooldown, 10, "Sprites/UI/ShopUI/ShieldCDT1.dds", "Extended Charger Cable Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_StormReaktor_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::ShieldDuration, 10, "Sprites/UI/ShopUI/ShieldDUT1.dds","Storm Reaktor Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_5T0NK5BatteryCore_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::ShieldHealth, 10, "Sprites/UI/ShopUI/ShieldHPT1.dds","5TONK% Battery Core Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_Triggerhappy_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::BasicAttackSpeed, 10, "Sprites/UI/ShopUI/BasicAttackSpeedT1.dds","Trigger Happy Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_ArkReaktor_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::MissileCooldown, 10, "Sprites/UI/ShopUI/MissileCDRT1.dds","Ark Reaktor Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_GrannysDoughRecipe_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::MissileDamage, 10, "Sprites/UI/ShopUI/MissileDMGT1.dds","Grannys Dough Recipe Tier 1"));
+	myTier1Buttons.push_back(new ShopButton("Sprites/UI/ShopUI/FinalCards/shopUI_cards_BluesprintEnlarger_Tier1.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier1Upgrades::MissileExplosionRadius, 10, "Sprites/UI/ShopUI/MissileRADT1.dds","Bluesprint Enlarger Tier 1"));
 	
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_AndAnotherOne_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::BasicAttackAdditionalProjectile, 10, "Sprites/UI/ShopUI/BasicAttackPlusOneT2.dds","And Another One"));
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_DrumMagazine_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::RapidFireDuration, 10, "Sprites/UI/ShopUI/RapidFireDUT2.dds","Drum Magazine Tier 2"));
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_ExtendedChargerCable_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::ShieldCooldown, 10, "Sprites/UI/ShopUI/ShieldCDT2.dds", "Extended Charger Cable Tier 2"));
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_Triggerhappy_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::BasicAttackSpeed, 10, "Sprites/UI/ShopUI/BasicAttackSpeedT2.dds","Trigger Happy Tier 2"));
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_ArkReaktor_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::MissileCooldown, 10, "Sprites/UI/ShopUI/MissileCDRT2.dds","Ark Reaktor Tier 2"));
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_GrannysDoughRecipe_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::MissileDamage, 10, "Sprites/UI/ShopUI/MissileDMGT2.dds","Grannys Dough Recipe Tier 2"));
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_BluesprintEnlarger_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::MissileExplosionRadius, 10, "Sprites/UI/ShopUI/MissileRADT2.dds", "Bluesprint Enlarger Tier 2"));
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_AdrenalineRush_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::RapidFireAttackSpeed, 10, "Sprites/UI/ShopUI/RapidFireAST2.dds","Adrenaline Rush Tier 2"));
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_StormReaktor_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::ShieldDuration, 10, "Sprites/UI/ShopUI/ShieldDUT2.dds","Storm Reaktor Tier 2"));
-	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_5T0NK5BatteryCore_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::ShieldHealth, 10, "Sprites/UI/ShopUI/ShieldHPT2.dds","5TONK% Battery Core Tier 2"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_AndAnotherOne_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::BasicAttackAdditionalProjectile, 20, "Sprites/UI/ShopUI/BasicAttackPlusOneT2.dds","And Another One"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_DrumMagazine_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::RapidFireDuration, 20, "Sprites/UI/ShopUI/RapidFireDUT2.dds","Drum Magazine Tier 2"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_ExtendedChargerCable_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::ShieldCooldown, 20, "Sprites/UI/ShopUI/ShieldCDT2.dds", "Extended Charger Cable Tier 2"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_Triggerhappy_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::BasicAttackSpeed, 20, "Sprites/UI/ShopUI/BasicAttackSpeedT2.dds","Trigger Happy Tier 2"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_ArkReaktor_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::MissileCooldown, 20, "Sprites/UI/ShopUI/MissileCDRT2.dds","Ark Reaktor Tier 2"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_GrannysDoughRecipe_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::MissileDamage, 20, "Sprites/UI/ShopUI/MissileDMGT2.dds","Grannys Dough Recipe Tier 2"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_BluesprintEnlarger_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::MissileExplosionRadius, 20, "Sprites/UI/ShopUI/MissileRADT2.dds", "Bluesprint Enlarger Tier 2"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_AdrenalineRush_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::RapidFireAttackSpeed, 20, "Sprites/UI/ShopUI/RapidFireAST2.dds","Adrenaline Rush Tier 2"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_StormReaktor_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::ShieldDuration, 20, "Sprites/UI/ShopUI/ShieldDUT2.dds","Storm Reaktor Tier 2"));
+	myTier2Buttons.push_back(new ShopButtonTier2("Sprites/UI/ShopUI/FinalCards/shopUI_cards_5T0NK5BatteryCore_Tier2.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier2Upgrades::ShieldHealth, 20, "Sprites/UI/ShopUI/ShieldHPT2.dds","5TONK% Battery Core Tier 2"));
+	
 
-	myTier3Buttons.push_back(new ShopButtonTier3("Sprites/UI/ShopUI/FinalCards/shopUI_cards_Triggerhappy_Tier3.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier3Upgrades::BasicAttackSpeed, 15, "Sprites/UI/ShopUI/BasicAttackSpeedT3.dds","Trigger Happy Tier 3"));
-	myTier3Buttons.push_back(new ShopButtonTier3("Sprites/UI/ShopUI/FinalCards/shopUI_cards_G3TR3-KT_Tier3.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier3Upgrades::MissileCluster, 15, "Sprites/UI/ShopUI/MissileT3.dds", "G3TR3-KT"));
-	myTier3Buttons.push_back(new ShopButtonTier3("Sprites/UI/ShopUI/FinalCards/shopUI_cards_RideTheLightning_Tier3.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier3Upgrades::RapidFirePenetrating, 15, "Sprites/UI/ShopUI/RapidFirePENT3.dds","Ride The Lightning"));
-	myTier3Buttons.push_back(new ShopButtonTier3("Sprites/UI/ShopUI/FinalCards/shopUI_cards_EMPoweredShieldReactor_Tier3.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier3Upgrades::ShieldExplosion, 15, "Sprites/UI/ShopUI/ShieldT3.dds","EMPowered Shield Reactor"));
+	myTier3Buttons.push_back(new ShopButtonTier3("Sprites/UI/ShopUI/FinalCards/shopUI_cards_Triggerhappy_Tier3.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier3Upgrades::BasicAttackSpeed, 30, "Sprites/UI/ShopUI/BasicAttackSpeedT3.dds","Trigger Happy Tier 3"));
+	myTier3Buttons.push_back(new ShopButtonTier3("Sprites/UI/ShopUI/FinalCards/shopUI_cards_G3TR3-KT_Tier3.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier3Upgrades::MissileCluster, 30, "Sprites/UI/ShopUI/MissileT3.dds", "G3TR3-KT"));
+	myTier3Buttons.push_back(new ShopButtonTier3("Sprites/UI/ShopUI/FinalCards/shopUI_cards_RideTheLightning_Tier3.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier3Upgrades::RapidFirePenetrating, 30, "Sprites/UI/ShopUI/RapidFirePENT3.dds","Ride The Lightning"));
+	myTier3Buttons.push_back(new ShopButtonTier3("Sprites/UI/ShopUI/FinalCards/shopUI_cards_EMPoweredShieldReactor_Tier3.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier3Upgrades::ShieldExplosion, 30, "Sprites/UI/ShopUI/ShieldT3.dds","EMPowered Shield Reactor"));
+	myTier3Buttons.push_back(new ShopButtonTier3("Sprites/UI/ShopUI/FinalCards/shopUI_cards_G3TR3-KT_Tier3.dds", { 0,0 }, { 1,1 }, { 0.5f,0.5f }, 500, Enums::Tier3Upgrades::MissileCluster, 30, "Sprites/UI/ShopUI/MissileT3.dds", "G3TR3-KT"));
 
 }
 
@@ -128,12 +130,6 @@ std::vector<Studio::ButtonElement*> Studio::ShopUI::GetShopButtons()
 		int temp = GetRandomNumberInRange(myTier2ButtonsTemp.size()-1);
 
 		ShopButtonTier2* buttonToPushBack = myTier2ButtonsTemp[temp];
-
-		while (PlayerAccessor::GetInstance()->GetAmountOfProjectiles() < 3 && buttonToPushBack->GetUpgradeType()==Enums::Tier2Upgrades::BasicAttackAdditionalProjectile)
-		{
-			temp = GetRandomNumberInRange(myTier2ButtonsTemp.size() - 1);
-			buttonToPushBack = myTier2ButtonsTemp[temp];
-		}
 
 		myTier2ButtonsTemp.erase(myTier2ButtonsTemp.begin() + temp);
 
