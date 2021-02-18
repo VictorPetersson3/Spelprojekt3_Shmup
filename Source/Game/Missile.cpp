e6e0ca680b8303fc262e8b334205b20ef530bfc4
jesper-lundgren <70626985+jesper-lundgren@users.noreply.github.com>
2021-02-18 12:31:44 +0100

    Fixed flame offset

--- Source/Game/Missile.cpp
+++ Source/Game/Missile.cpp
@@ -67,6 +67,8 @@ namespace Studio
 			mySpriteSheet.GetSprite()->SetTextureRect(1.0f, 0.0f, 0.0f, 1.0f);
 
 			myIsReversed = true;
+
+			myFlame.GetSpriteSheet().SetSizeRelativeToImage({ -1.0f, 0.25f });
 		}
 	}
 
@@ -92,9 +94,9 @@ namespace Studio
 			}
 			else
 			{
-				myVelocity.x += myAccelerationSpeed;
+				myFlame.Update(4, { myPosition.x - 23.0f, myPosition.y });
 
-				myFlame.Update(4, { myPosition.x - 23.0f, myPosition.y + myVelocity.y });
+				myVelocity.x += myAccelerationSpeed;
 			}
 		}
 		else
@@ -112,9 +114,9 @@ namespace Studio
 			}
 			else
 			{
-				myVelocity.x += myAccelerationSpeed;
-
 				myFlame.Update(1, { myPosition.x + 23.0f, myPosition.y + myVelocity.y });
+				
+				myVelocity.x += myAccelerationSpeed;
 			}
 		}
 
